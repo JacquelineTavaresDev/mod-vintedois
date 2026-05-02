567ab2a4bbd40ddf18c5cf5f5993d0ba42a14e01
JacquelineTavares <jacqueline.tavares1996@gmail.com>
2026-05-02 15:45:22 -0300

    Import Image component from next/image

--- src/app/filmes/[id]/page.tsx
+++ src/app/filmes/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from 'next/navigation';
 import Link from 'next/link';
+import Image from 'next/image';
 import styles from './DetalheFilme.module.css';
 import { getMovieDetails } from '@/src/lib/api/tmdb';
 
